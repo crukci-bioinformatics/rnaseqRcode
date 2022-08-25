0ad30d93788b189fbf7c2aadb69df9a9854f2460
Chandra Chilamakuri <chandu.ncbs@gmail.com>
2022-08-25 13:52:15 +0100

    Modified such that returns list of plots.

--- R/normFactorsBoxplot.R
+++ R/normFactorsBoxplot.R
@@ -2,7 +2,7 @@
 #'
 #' @param dds a DESeqDataSet object; dds after applying estimateSizeFactors
 #'
-#' @return An object created by \code{ggplot}
+#' @return A list that contains two plots created by \code{ggplot}
 #' @export normFactorsBoxplot
 #'
 #' @examples
@@ -16,45 +16,33 @@
 
 normFactorsBoxplot <- function(dds){
   assert_that(is(dds, 'DESeqDataSet'), msg = 'Object is not DESeqDataSet')
-  s_sheet <- colData(dds) %>%
-    as.data.frame()
 
-  normFactors <- normalizationFactors(dds) %>%
+  s_sheet <- colData(dds) %>%
     as.data.frame() %>%
-    rownames_to_column(var='gene_id') %>%
-    pivot_longer(cols = -gene_id, names_to = 'SampleName', values_to = 'normFactors') %>%
-    left_join(s_sheet, by='SampleName')
+    arrange(SampleGroup)
 
   rawCounts <- counts(dds, normalized = FALSE) %>%
     as.data.frame() %>%
     rownames_to_column(var='gene_id') %>%
     pivot_longer(cols = -gene_id, names_to = 'SampleName', values_to = 'rawCounts') %>%
-    left_join(s_sheet, by='SampleName')
+    left_join(s_sheet, by='SampleName') %>%
+    mutate(SampleName = factor(SampleName, levels = s_sheet$SampleName))
 
   normCounts <- counts(dds, normalized = TRUE) %>%
     as.data.frame() %>%
     rownames_to_column(var='gene_id') %>%
     pivot_longer(cols = -gene_id, names_to = 'SampleName', values_to = 'normCounts') %>%
-    left_join(s_sheet, by='SampleName')
+    left_join(s_sheet, by='SampleName') %>%
+    mutate(SampleName = factor(SampleName, levels = s_sheet$SampleName))
 
-  plotTheme <- theme(
+  plotTheme <- theme_classic() +
+    theme(
     panel.background = element_blank(),
-    axis.text = element_text(color='blue'),
     axis.text.x = element_blank(),
     axis.ticks.x = element_blank(),
     legend.position = 'none',
-    plot.title = element_text(color='brown', face='bold', size=10, hjust = 0.5)
+    plot.title = element_text(color='black', face='bold', size=10, hjust = 0.5)
   )
-
-
-  pNormFactors <- ggplot(normFactors, aes(x=SampleName, y=normFactors, color=SampleGroup)) +
-    geom_boxplot(outlier.shape = NA) +
-    labs(
-      x='',
-      y='Factors',
-      title = 'Normalisation Factor Distribution'
-    ) +
-    plotTheme
 
   pRawCounts <- ggplot(rawCounts, aes(x=SampleName, y=log2(rawCounts+1), color=SampleGroup)) +
     geom_boxplot(outlier.shape = NA) +
@@ -74,9 +62,12 @@ normFactorsBoxplot <- function(dds){
     ) +
     plotTheme +
     theme( legend.position = 'bottom',
-           axis.text.x = element_text(color='blue', angle = 90)
+           axis.text.x = element_text(angle = 90)
            )
 
-  pNormFactors / pRawCounts /  pNormCounts
+  return(list( pRawCounts=pRawCounts,
+               pNormCounts=pNormCounts
+               )
+         )
 
 }
